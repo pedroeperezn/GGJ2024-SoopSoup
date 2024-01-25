a3aa72c8689b688b843dd62b41a60964290e4e73
Connor deBoer <pg24connor@vfs.com>
2024-01-24 16:03:41 -0800

    Hover sorta working

--- Assets/Scripts/Character/SpinTongue.cs
+++ Assets/Scripts/Character/SpinTongue.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 
 public class SpinTongue : MonoBehaviour
 {
-    [SerializeField] private Transform _head;
+    [SerializeField] private AnimationCurve _hoverCurve;
+    [SerializeField] private Rigidbody2D _head;
+    [SerializeField] ManageBodyWeight _body;
+    private bool _grounded = false;
+    private Coroutine _hoverCoroutine;
+
     internal void Spin()
     {
         //TODO: Play animation/ make a hinge joint motor spin
@@ -13,10 +18,24 @@ public class SpinTongue : MonoBehaviour
     }
     internal void TryHover()
     {
+        if (_grounded) return;
         //TODO: Add a force from the face up
         Debug.Log("Add a force from the face up");
-        
+        _hoverCoroutine = StartCoroutine(Hover());
     }
+
+    private IEnumerator Hover()
+    {
+        _body.IsFlying = true;
+        float timeElapse = 0;
+        while(!_grounded)
+        {
+            yield return null;
+            _head.AddForce(Vector2.up * _hoverCurve.Evaluate(timeElapse) * 1000);
+            timeElapse += Time.deltaTime;
+        }
+    }
+
     internal void StopSpinning()
     {
         //TODO: Stop playing the animation/Turn off the motor
@@ -26,6 +45,8 @@ public class SpinTongue : MonoBehaviour
     {
         //TODO: Start a timer to reset the cooldown
         Debug.Log("Start a timer to reset the cooldown");
+        StopCoroutine(_hoverCoroutine);
+        _body.IsFlying = false;
     }
 
 }
