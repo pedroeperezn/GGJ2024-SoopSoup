0cbc640754349f711ac11897f0890e3948abeceb
Connor deBoer <pg24connor@vfs.com>
2024-01-27 16:03:32 -0800

    Added some comments

--- Assets/Scripts/UI/UpIndicatorUIHandler.cs
+++ Assets/Scripts/UI/UpIndicatorUIHandler.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Hey so what I ended up doing here is firing some events from the spin tongue and using coroutines, 
+/// keeping track of the state so that if the player lets go in the middle of a hover we can stop the 
+/// drain coroutine and start the fill one without having to worry, we could if we wanted change it 
+/// so that the fill starts at where the drain left it, but honestly I'm being pretty lazy and I think 
+/// it looks fine for now
+/// </summary>
 public class UpIndicatorUIHandler : MonoBehaviour
 {
+    // grab the image and animation curve cause easy
     [SerializeField] private Image _image;
     [SerializeField] private AnimationCurve _fillCurve;
     private SpinTongue _spinTongue => FindAnyObjectByType<SpinTongue>();
@@ -17,7 +25,6 @@ public class UpIndicatorUIHandler : MonoBehaviour
         _spinTongue.StartRecharge.AddListener(StartFilling);
         _spinTongue.StartHover.AddListener(StartDraining);
     }
-
     private void StartFilling()
     {
         if (_currentTask != null) StopCoroutine(_currentTask);
@@ -38,7 +45,6 @@ public class UpIndicatorUIHandler : MonoBehaviour
             _image.fillAmount = _fillCurve.Evaluate(percentComplete);
         }
     }
-
     private IEnumerator Drain()
     {
         if (!_spinTongue) yield return null;
