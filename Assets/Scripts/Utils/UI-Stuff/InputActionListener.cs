191e15b8b5fba7c99424b1f66361372a6df564ef
Connor deBoer <pg24connor@vfs.com>
2024-01-27 18:48:27 -0800

    Half way through

---
+++ Assets/Scripts/Utils/UI-Stuff/InputActionListener.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+namespace UIComponents
+{
+    public class InputActionListener : MonoBehaviour
+    {
+        [SerializeField] private InputActionReference _action;
+        [SerializeField] private Button _activateButton;
+
+        public UnityEvent OnPerformed;
+
+        private void OnEnable()
+        {
+            if(_action != null) _action.action.performed += Performed;
+        }
+
+        private void OnDisable()
+        {
+            if(_action != null) _action.action.performed -= Performed;
+        }
+
+        private void Performed(InputAction.CallbackContext ctx)
+        {
+            OnPerformed.Invoke();
+            _activateButton?.onClick.Invoke();
+        }
+
+        public void ForcePerform()
+        {
+            OnPerformed.Invoke();
+        }
+    }
+}
